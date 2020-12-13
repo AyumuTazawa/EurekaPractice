0032ccb852514b67d36eaf704c1ab6b0264c65b8
AyumuTazawa <tazawaayumu@gmail.com>
2020-12-13 18:57:51 +0900

    test

--- EurekaPractice/MyFormViewController.swift
+++ EurekaPractice/MyFormViewController.swift
@@ -7,32 +7,32 @@
 //
 
 import UIKit
-import Eureka
+//import Eureka
 
-class MyFormViewController: FormViewController {
-
-    var onWiFi : Bool = false
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        form +++ Section(header: "名前", footer: "名前設定")
-        <<< TextRow("Name"){
-            $0.title = "名前を入力"
-            $0.placeholder = "フルネームを書いてください"
-            }
-        form +++ Section("ネットワーク設定")
-                    <<< SwitchRow(){ row in
-                        row.title = "Wi-Fi"
-                        row.value = true
-                        }.onChange{[unowned self] row in
-                    self.onWiFi = row.value!
-                    print(self.onWiFi)
-                        }
-
-        print("起動完了")
-    }
+//class MyFormViewController: FormViewController {
+//
+//    var onWiFi : Bool = false
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        form +++ Section(header: "名前", footer: "名前設定")
+//        <<< TextRow("Name"){
+//            $0.title = "名前を入力"
+//            $0.placeholder = "フルネームを書いてください"
+//            }
+//        form +++ Section("ネットワーク設定")
+//                    <<< SwitchRow(){ row in
+//                        row.title = "Wi-Fi"
+//                        row.value = true
+//                        }.onChange{[unowned self] row in
+//                    self.onWiFi = row.value!
+//                    print(self.onWiFi)
+//                        }
+//
+//        print("起動完了")
+//    }
 
     
 
-}
+//}
